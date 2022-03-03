02058a299c196d2180f737d8d953f632c628f039
William Burton <twitchifygamers@gmail.com>
2022-03-03 12:29:13 +0000

    Added line count

--- main.cpp
+++ main.cpp
@@ -6,12 +6,17 @@ int main()
 {
     std::string outPut;
 
+    int count = 0;
 
-    std::ifstream MyFile("/tests/helloworld.txt");
+    std::ifstream MyFile("./tests/helloworld.txt");
 
     while(getline(MyFile, outPut))
     {
+        ++count;
+        std::cout << count << "| ";
         std::cout << outPut << std::endl;
     }
+
+    std::cout << "Lines of code: " << count << std::endl;
 
 }
